e9dc34916c8f7274215253f20e93faee30a49fc8
mp4253 <111766615+mp4253@users.noreply.github.com>
2023-06-27 13:39:16 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,24 @@
+function myFunc(){
+
+    var a = document.querySelector('p').innerHTML="This is a Query Selector";
+
+    var b = "Meet";
+    var c = document.getElementById("messages").innerHTML=`
+    <div> This is performed by JS on clicking the button by ${b}.</div>
+
+    <ol>
+        <li>Meet</li>
+        <li>Het</li>
+        <li>Vishal</li>
+        <li>Parth Amreliya</li>
+        <li>Shani</li>
+        <li>Krutak</li>
+        <li>Jainish</li>
+        <li>Shivam</li>
+        <li>Parekh Mohit</li>
+        <li>Ritik</li>
+    </ol>
+    `;
+     
+
+}
